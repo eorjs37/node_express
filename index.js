a7fd610ee1a08c754eac24d4857e09737c97174b
maxgunchoi <maxgunchoi@carrotglobal.com>
2022-03-15 08:13:37 +0900

    커밋

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 let sqlite3 = require('sqlite3');
+const sqlite3 = require('sqlite3');
+const express = require('express');
+const cors = require('cors');
+
+const PORT = 8000;
+
+
+let db = new sqlite3.Database('database.db',(err) =>{
+    if(!err){
+        db.run('CREATE TABLE tbl_about_myself (name text,email text',(err2)=>{
+            if(!err2){
+                db.run(
+                    "INSERT INTO tbl_about_myself (name,email) VALUES('DOPT','armigar@naver.com')"
+                )
+            }
+        })
+    }
+});
