dd82b148df599c193a0c77ec4e24219d6d3d24cd
maxgunchoi <maxgunchoi@carrotglobal.com>
2022-03-21 08:44:00 +0900

    tbl_blog 생성 및 GET

--- get.js
+++ get.js
@@ -69,4 +69,22 @@ module.exports.setup = function(app,db){
             }
         )
     })
+
+    app.get('/db/blog',(req,res,next)=>{
+        let result = {
+            rsp: 'fail',
+        };
+
+        db.all(`SELECT * FROM tbl_blog order by id desc`, (err,rows)=>{
+            if(!err){
+                result.rsp ='ok';
+                result.data = rows;
+                res.json(result);
+            }
+            else{
+                result.err = err.message;
+                res.json(result);
+            }
+        })
+    })
 }
